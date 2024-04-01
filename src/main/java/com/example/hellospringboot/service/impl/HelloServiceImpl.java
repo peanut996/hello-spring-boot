c34fc04f2753f6dfb48f54ca12fdc81e804be448
peanut996 <849421294godw@gmail.com>
2024-04-02 01:36:52 +0800

    add transactional.

--- src/main/java/com/example/hellospringboot/service/impl/HelloServiceImpl.java
+++ src/main/java/com/example/hellospringboot/service/impl/HelloServiceImpl.java
@@ -2,10 +2,14 @@ package com.example.hellospringboot.service.impl;
 
 import com.example.hellospringboot.service.HelloService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class HelloServiceImpl implements HelloService {
+
     @Override
+    @Transactional(propagation = Propagation.REQUIRED)
     public String ping() {
         return "pong";
     }
