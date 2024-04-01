24fa6db8ed961b0b0f192b33ee0b8652df2bcec5
peanut996 <849421294godw@gmail.com>
2024-04-02 00:21:37 +0800

    add more advice .

--- src/main/java/com/example/hellospringboot/aop/ExecutionTimeAspect.java
+++ src/main/java/com/example/hellospringboot/aop/ExecutionTimeAspect.java
@@ -2,9 +2,9 @@ package com.example.hellospringboot.aop;
 
 
 import com.example.hellospringboot.annotation.MeasureExecutionTime;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -14,6 +14,13 @@ import org.springframework.stereotype.Component;
 @Component
 public class ExecutionTimeAspect {
     private static final Logger logger = LoggerFactory.getLogger(ExecutionTimeAspect.class);
+
+    @Before("@annotation(com.example.hellospringboot.annotation.MeasureExecutionTime)")
+    public void logMethodAccessBefore(JoinPoint joinPoint) {
+        String methodName = joinPoint.getSignature().getName();
+        String className = joinPoint.getSignature().getDeclaringTypeName();
+        logger.info("Before execution of [{}.{}]", className, methodName);
+    }
 
     @Around("@annotation(com.example.hellospringboot.annotation.MeasureExecutionTime)")
     public Object measureExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
@@ -26,5 +33,27 @@ public class ExecutionTimeAspect {
                 endTime - startTime);
 
         return result;
+    }
+
+    @After("@annotation(com.example.hellospringboot.annotation.MeasureExecutionTime)")
+    public void logMethodAccessAfter(JoinPoint joinPoint) {
+        String methodName = joinPoint.getSignature().getName();
+        String className = joinPoint.getSignature().getDeclaringTypeName();
+        logger.info("After execution of [{}.{}]", className, methodName);
+    }
+    @AfterReturning(pointcut = "@annotation(com.example.hellospringboot.annotation.MeasureExecutionTime)",
+            returning = "retVal")
+    public void logMethodAccessAfterReturning(JoinPoint joinPoint, Object retVal) {
+        String methodName = joinPoint.getSignature().getName();
+        String className = joinPoint.getSignature().getDeclaringTypeName();
+        logger.info("After returning from [{}.{}] with return value: {}", className, methodName, retVal);
+    }
+
+    @AfterThrowing(pointcut = "@annotation(com.example.hellospringboot.annotation.MeasureExecutionTime)",
+            throwing = "ex")
+    public void logMethodAccessAfterThrowing(JoinPoint joinPoint, Throwable ex) {
+        String methodName = joinPoint.getSignature().getName();
+        String className = joinPoint.getSignature().getDeclaringTypeName();
+        logger.error("After throwing from [{}.{}] with exception: {}", className, methodName, ex.getMessage());
     }
 }
