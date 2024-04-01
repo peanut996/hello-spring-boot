35bc1368deeaef7b11ca332abc7f815c252ef7f1
peanut996 <849421294godw@gmail.com>
2024-04-02 00:39:40 +0800

    add lombok .

--- src/main/java/com/example/hellospringboot/controller/Hello.java
+++ src/main/java/com/example/hellospringboot/controller/Hello.java
@@ -1,13 +1,19 @@
 package com.example.hellospringboot.controller;
 
 import com.example.hellospringboot.annotation.MeasureExecutionTime;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.*;
 
 @RestController
+@RequiredArgsConstructor
 public class Hello {
+
+    public final ApplicationContext applicationContext;
 
     @GetMapping("/ping")
     @MeasureExecutionTime
