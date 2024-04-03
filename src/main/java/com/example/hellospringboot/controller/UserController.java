20134a93075b69c90b9ea192756845a7e45936e6
peanut996 <849421294godw@gmail.com>
2024-04-03 17:02:08 +0800

    use discovery client.

--- src/main/java/com/example/hellospringboot/controller/UserController.java
+++ src/main/java/com/example/hellospringboot/controller/UserController.java
@@ -19,7 +19,7 @@ public class UserController {
 
     @GetMapping("/users")
     @MeasureExecutionTime
-    List<User> ping() {
+    List<User> list() {
         return userService.list();
     }
 }
