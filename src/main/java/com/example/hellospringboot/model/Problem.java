4f1ad96144d637a7d153f5f64f74f4c11da2807e
peanut996 <849421294godw@gmail.com>
2024-04-08 20:02:39 +0800

    problem: 2009
    https://leetcode.cn/problems/minimum-number-of-operations-to-make-array-continuous

--- src/main/java/com/example/hellospringboot/model/Problem.java
+++ src/main/java/com/example/hellospringboot/model/Problem.java
@@ -4,6 +4,34 @@ package com.example.hellospringboot.model;
 import lombok.Data;
 import org.springframework.stereotype.Component;
 
+import java.util.*;
+
 @Data
 public class Problem {
+    /**
+     * 2009. 使数组连续的最少操作数
+     * 思路：滑动窗口
+     * https://leetcode.cn/problems/minimum-number-of-operations-to-make-array-continuous/
+     */
+    public int minOperations(int[] nums) {
+        int len = nums.length;
+        Set<Integer> set = new HashSet<>();
+        for (int n : nums) {
+            set.add(n);
+        }
+        List<Integer> list = new ArrayList<>(set);
+        Collections.sort(list);
+        int res = len;
+        int j = 0;
+        for (int i = 0; i < list.size(); i++) {
+            int left = list.get(i);
+            int right = nums.length - 1 + left;
+            while (j < list.size() && list.get(j) <= right) {
+                res = Math.min(res, len - (j - i + 1));
+                j++;
+            }
+        }
+        return res;
+    }
+
 }
