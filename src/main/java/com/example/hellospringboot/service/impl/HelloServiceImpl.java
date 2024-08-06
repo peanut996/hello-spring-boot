a94c243fc19809baba9338d26a5a5ca25959dd7f
peanut996 <peanut996@icloud.com>
2024-08-07 00:53:06 +0800

    add gRPC.

---
+++ src/main/java/com/example/hellospringboot/service/impl/HelloServiceImpl.java
@@ -0,0 +1,23 @@
+package com.example.hellospringboot.service.impl;
+
+import com.example.grpc.HelloRequest;
+import com.example.grpc.HelloResponse;
+import com.example.grpc.HelloServiceGrpc;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@GrpcService
+public class HelloServiceImpl extends HelloServiceGrpc.HelloServiceImplBase {
+    @Override
+    public void sayHello(HelloRequest request, StreamObserver<HelloResponse> responseObserver) {
+        String name = request.getName();
+        String message = "Hello, " + name;
+
+        HelloResponse response = HelloResponse.newBuilder()
+                .setMessage(message)
+                .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+}
