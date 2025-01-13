aabf14987e6352c22a8e8ed45ca4f545fc01ea45
peanut996 <peanut996@icloud.com>
2025-01-13 22:29:25 +0800

    add medium 128.

--- src/main/java/com/example/hellospringboot/model/Problem.java
+++ src/main/java/com/example/hellospringboot/model/Problem.java
@@ -704,4 +704,33 @@ public class Problem {
         }
         return 0;
     }
+
+
+    /**
+     * 给定一个未排序的整数数组 nums ，找出数字连续的最长序列（不要求序列元素在原数组中连续）的长度。
+     * 请你设计并实现时间复杂度为 O(n) 的算法解决此问题。
+     */
+    @Medium(point = Point.HASH,
+            source = "https://leetcode.cn/problems/longest-consecutive-sequence/?envType=study-plan-v2&envId=top-100-liked",
+            title = "128. 最长连续序列")
+    public int longestConsecutive(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+        int longest = 0;
+        for (int num : set) {
+            if (!set.contains(num - 1)) {
+                int cur = 1;
+                int curNum = num;
+                while (set.contains(curNum + 1)) {
+                    cur++;
+                    curNum++;
+                }
+
+                longest = Math.max(longest, cur);
+            }
+        }
+        return longest;
+    }
 }
