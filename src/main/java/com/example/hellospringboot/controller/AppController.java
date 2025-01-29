2ab98be02861933ff030895247c76787fea3f5ce
peanut996 <peanut996@icloud.com>
2025-01-29 21:39:44 +0800

    fix deps

--- src/main/java/com/example/hellospringboot/controller/AppController.java
+++ src/main/java/com/example/hellospringboot/controller/AppController.java
@@ -1,8 +1,6 @@
 package com.example.hellospringboot.controller;
 
 import com.example.hellospringboot.annotation.MeasureExecutionTime;
-import com.netflix.discovery.EurekaClient;
-import com.netflix.discovery.shared.Applications;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,13 +8,4 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 public class AppController {
-
-
-    private final EurekaClient discoveryClient;
-
-    @GetMapping("/apps")
-    @MeasureExecutionTime
-    Applications list() {
-        return discoveryClient.getApplications();
-    }
 }
