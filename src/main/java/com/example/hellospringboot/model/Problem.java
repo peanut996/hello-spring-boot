98ae61f1fb783c523f328a70f71b372fb24c3c57
peanut996 <peanut996@icloud.com>
2025-01-30 22:56:24 +0800

    add medium 48

--- src/main/java/com/example/hellospringboot/model/Problem.java
+++ src/main/java/com/example/hellospringboot/model/Problem.java
@@ -1199,4 +1199,29 @@ public class Problem {
         }
         return res;
     }
+
+    @Medium(
+        title = "48. 旋转图像",
+        point = Point.ARRAY,
+        source = "https://leetcode.cn/problems/rotate-image"
+    )
+    public void rotate(int[][] matrix) {
+        int len = matrix.length;
+
+        for (int i = 0; i < len; i++) {
+            for (int j = i; j < len; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = tmp;
+            }
+        }
+
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < len / 2; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[i][len - 1 - j];
+                matrix[i][len - 1 - j] = tmp;
+            }
+        }
+    }
 }
