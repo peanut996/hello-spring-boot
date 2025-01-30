018d91957831c4f5af4612df9644ea5811b2156a
peanut996 <peanut996@icloud.com>
2025-01-30 23:26:04 +0800

    add medium 240

--- README.md
+++ README.md
@@ -1,9 +1,26 @@
-# Hello Spring Boot 
+# Hello Spring Boot
 
-Playground for java spring boot and more. 
+Playground for java spring boot and more.
 
 
 Include:
 + Spring Boot
 + Mybatis
 + H2
+
+
+```mermaid
+graph LR
+    A[开始, 从右上角开始] --> B{当前元素 == 目标值?};
+    B -- 是 --> C[返回 true];
+    B -- 否 --> D{当前元素 > 目标值?};
+    D -- 是 --> E[向左移动];
+    E --> F{是否越界？};
+     F -- 是 --> G[返回 false];
+     F -- 否 --> B;
+    D -- 否 --> H[向下移动];
+    H --> I{是否越界？};
+     I -- 是 --> G;
+     I -- 否 --> B;
+
+```
--- src/main/java/com/example/hellospringboot/model/Problem.java
+++ src/main/java/com/example/hellospringboot/model/Problem.java
@@ -1224,4 +1224,28 @@ public class Problem {
             }
         }
     }
+
+    @Medium(
+        title = "240. 搜索二维矩阵 II",
+        source = "https://leetcode.cn/problems/search-a-2d-matrix-ii/",
+        point = { Point.ARRAY }
+    )
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix[0] == null) return false;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int row = 0;
+        int col = n - 1;
+        while (row < m && col > -1) {
+            int cur = matrix[row][col];
+            if (cur == target) {
+                return true;
+            } else if (cur > target) {
+                col -= 1;
+            } else if (cur < target) {
+                row += 1;
+            }
+        }
+        return false;
+    }
 }
