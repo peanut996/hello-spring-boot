2cf74f967607d7fe0c72ee09a1a6d925b76a8b29
peanut996 <peanut996@icloud.com>
2025-01-31 23:00:31 +0800

    add medium 2

--- src/main/java/com/example/hellospringboot/model/Problem.java
+++ src/main/java/com/example/hellospringboot/model/Problem.java
@@ -1470,4 +1470,31 @@ public class Problem {
 
         return dummy.next;
     }
+
+    @LeetCode(
+        level = Level.MEDIUM,
+        title = "2. 两数相加",
+        source = "https://leetcode.cn/problems/add-two-numbers/",
+        point = { Point.LINKED_LIST }
+    )
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode();
+        ListNode current = dummy;
+        int carry = 0;
+        while (l1 != null || l2 != null || carry != 0) {
+            int sum = carry;
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            carry = sum / 10;
+            current.next = new ListNode(sum % 10);
+            current = current.next;
+        }
+        return dummy.next;
+    }
 }
