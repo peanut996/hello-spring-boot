48fe799c5310c1b8cb6e142a3e186b4616b07bee
peanut996 <peanut996@icloud.com>
2025-01-31 13:17:41 +0800

    add new annotation
    Signed-off-by: peanut996 <peanut996@icloud.com>

---
+++ src/main/java/com/example/hellospringboot/annotation/LeetCode.java
@@ -0,0 +1,25 @@
+package com.example.hellospringboot.annotation;
+
+import com.example.hellospringboot.enumerate.Point;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.SOURCE)
+@Target(value = { ElementType.TYPE, ElementType.METHOD })
+public @interface LeetCode {
+    enum Level {
+        EASY,
+        MEDIUM,
+        HARD,
+    }
+
+    Level level();
+
+    Point[] point() default {};
+
+    String source() default "";
+
+    String title() default "";
+}
