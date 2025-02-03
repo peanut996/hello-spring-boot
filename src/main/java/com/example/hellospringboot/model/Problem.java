1d2bbc8a2da3cc657a43d8076393ce21c59f05ed
peanut996 <peanut996@icloud.com>
2025-02-03 21:43:55 +0800

    add easy 101
    Signed-off-by: peanut996 <peanut996@icloud.com>

--- src/main/java/com/example/hellospringboot/model/Problem.java
+++ src/main/java/com/example/hellospringboot/model/Problem.java
@@ -1809,4 +1809,30 @@ public class Problem {
 
         return root;
     }
+
+    @LeetCode(
+        level = Level.EASY,
+        title = "101. 对称二叉树",
+        source = "https://leetcode.cn/problems/symmetric-tree/",
+        point = { Point.BINARY_TREE, Point.RECURSION }
+    )
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+
+        return isSymmetricHelper(root.left, root.right);
+    }
+
+    boolean isSymmetricHelper(TreeNode left, TreeNode right) {
+        if (left == null || right == null) {
+            return left == right;
+        }
+
+        return (
+            left.val == right.val &&
+            isSymmetricHelper(left.right, right.left) &&
+            isSymmetricHelper(left.left, right.right)
+        );
+    }
 }
