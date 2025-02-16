edd0cb66eb357ecbe7af5974e811966592da8876
peanut996 <peanut996@icloud.com>
2025-02-16 18:13:27 +0800

    add medium 139
    Signed-off-by: peanut996 <peanut996@icloud.com>

--- src/main/java/com/example/hellospringboot/model/Problem.java
+++ src/main/java/com/example/hellospringboot/model/Problem.java
@@ -2862,6 +2862,7 @@ public class Problem {
         point = { Point.STACK }
     )
     public String decodeString(String s) {
+        s.sub
         int mul = 0;
         StringBuilder res = new StringBuilder();
 
@@ -3181,4 +3182,29 @@ public class Problem {
         // 如果最终结果为最大值，则返回 -1 ，否则返回结果
         return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
     }
+
+    @LeetCode(
+            level = Level.MEDIUM,
+            title = "139. 单词拆分",
+            source = "https://leetcode.cn/problems/word-break/",
+            point = { Point.DYNAMIC_PROGRAMMING }
+        )
+        public boolean wordBreak(String s, List<String> wordDict) {
+            Set<String> wordSet = new HashSet<>(wordDict);
+            int length = s.length();
+            boolean[] dp = new boolean[length + 1];
+            dp[0] = true;
+
+            for (int i = 1; i <= length; i++) {
+                for (int j = 0; j < i; j++) {
+                    // 如果前面的子串可以拆分且当前子串在字典中，则当前位置可拆分
+                    if (dp[j] && wordSet.contains(s.substring(j, i))) {
+                        dp[i] = true;
+                        break;
+                    }
+                }
+            }
+            return dp[length];
+        }
+
 }
