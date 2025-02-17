257b4a076ca1b703af36db347906c3520f6fbf2d
peanut996 <peanut996@icloud.com>
2025-02-18 01:12:32 +0800

    add medium 64

--- src/main/java/com/example/hellospringboot/model/Problem.java
+++ src/main/java/com/example/hellospringboot/model/Problem.java
@@ -3353,4 +3353,33 @@ public class Problem {
 
         return maxLen;
     }
+
+    @LeetCode(
+        level = Level.MEDIUM,
+        title = "64. 最小路径和",
+        source = "https://leetcode.cn/problems/minimum-path-sum/",
+        point = { Point.DYNAMIC_PROGRAMMING }
+    )
+    public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
+        dp[0][0] = grid[0][0];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) {
+                    dp[i][j] = grid[0][0];
+                } else if (j == 0) {
+                    dp[i][j] = dp[i - 1][j] + grid[i][j];
+                } else if (i == 0) {
+                    dp[i][j] = dp[i][j - 1] + grid[i][j];
+                } else {
+                    // 选择上方或左方路径和较小的那个加上当前位置的值
+                    dp[i][j] =
+                        Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
 }
