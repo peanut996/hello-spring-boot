40fff9e53f576fe77c047796703f34cae1ea036a
peanut996 <peanut996@icloud.com>
2025-02-25 22:26:44 +0800

    add hard 41

--- src/main/java/com/example/hellospringboot/model/Problem.java
+++ src/main/java/com/example/hellospringboot/model/Problem.java
@@ -3635,4 +3635,38 @@ public class Problem {
 
         return maxVals.stream().mapToInt(i -> i).toArray();
     }
+
+    @LeetCode(
+        level = Level.HARD,
+        title = "41. 缺失的第一个正数",
+        source = "https://leetcode.cn/problems/first-missing-positive/",
+        point = { Point.ARRAY, Point.HASH }
+    )
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+
+        // 循环遍历数组
+        for (int i = 0; i < n; i++) {
+            // 将每个元素放到对应的位置上
+            // nums[i] >= 1 && nums[i] <= n 保证数字在 1 到 n 的范围内
+            // nums[nums[i] - 1] != nums[i] 避免重复交换
+            while (
+                nums[i] >= 1 && nums[i] <= n && nums[nums[i] - 1] != nums[i]
+            ) {
+                int temp = nums[nums[i] - 1];
+                nums[nums[i] - 1] = nums[i];
+                nums[i] = temp;
+            }
+        }
+
+        // 再次遍历数组，找到第一个不在正确位置上的数字
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+
+        // 如果所有数字都在正确位置上，则缺失的第一个正数是 n+1
+        return n + 1;
+    }
 }
