1eb83d94af3a90cd8cf3f7c0da4e25f93c01ba38
peanut996 <peanut996@icloud.com>
2025-03-07 17:02:58 +0800

    add mergeKSortedArray.

--- src/main/java/com/example/hellospringboot/model/Main.java
+++ src/main/java/com/example/hellospringboot/model/Main.java
@@ -1,5 +1,6 @@
 package com.example.hellospringboot.model;
 
+import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class Main {
@@ -15,5 +16,22 @@ public class Main {
             System.out.println(a + b);
             System.out.println();
         }
+    }
+
+    public int[] mergeKSortedArrays(int[][] arrays) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for (int[] array : arrays) {
+            for (int i : array) {
+                minHeap.offer(i);
+            }
+        }
+
+        int size = minHeap.size();
+        int[] res = new int[size];
+        for (int i = 0; i < size; i++) {
+            res[i] = minHeap.poll();
+        }
+
+        return res;
     }
 }
--- src/test/java/com/example/hellospringboot/model/ProblemTest.java
+++ src/test/java/com/example/hellospringboot/model/ProblemTest.java
@@ -1,10 +1,10 @@
 package com.example.hellospringboot.model;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class ProblemTest {
 
@@ -17,44 +17,43 @@ class ProblemTest {
 
     @Test
     void minOperations() {
-        int[] nums = new int[]{1, 2, 3, 5, 6};
+        int[] nums = new int[] { 1, 2, 3, 5, 6 };
         Assertions.assertEquals(1, problem.minOperations(nums));
 
-        nums = new int[]{2, 4, 3, 5};
+        nums = new int[] { 2, 4, 3, 5 };
         Assertions.assertEquals(0, problem.minOperations(nums));
 
-        nums = new int[]{1, 10, 100, 1000};
+        nums = new int[] { 1, 10, 100, 1000 };
         Assertions.assertEquals(3, problem.minOperations(nums));
     }
 
     @Test
     void maximumCount() {
-        int[] nums = new int[]{-2, -1, -1, 1, 2, 3};
+        int[] nums = new int[] { -2, -1, -1, 1, 2, 3 };
         assertEquals(3, problem.maximumCount(nums));
     }
 
     @Test
     void numSmallerByFrequency() {
-        String[] queries = new String[]{"cbd"};
-        String[] words = new String[]{"zaaaz"};
+        String[] queries = new String[] { "cbd" };
+        String[] words = new String[] { "zaaaz" };
 
         int[] res = problem.numSmallerByFrequency(queries, words);
-        assertArrayEquals(new int[]{1}, res);
+        assertArrayEquals(new int[] { 1 }, res);
     }
-
 
     @Test
     void threeSum() {
-        int[] nums = new int[]{-1, 0, 1, 2, -1, -4};
+        int[] nums = new int[] { -1, 0, 1, 2, -1, -4 };
         problem.threeSum(nums);
     }
 
     @Test
     void LRU() {
         LRUCache cache = new LRUCache(1);
-        cache.put(2,1);
-        assert  1 == cache.get(2);
-        cache.put(3,2);
+        cache.put(2, 1);
+        assert 1 == cache.get(2);
+        cache.put(3, 2);
         assert -1 == cache.get(2);
         assert 2 == cache.get(3);
     }
@@ -65,5 +64,14 @@ class ProblemTest {
         assert "aaabcbc".equals(res);
 
         assert "accaccacc".equals(problem.decodeString("3[a2[c]]"));
+    }
+
+    @Test
+    void testMergeKSortedArrays() {
+        Main main = new Main();
+        int[][] arrays = { { 1, 3, 5 }, { 2, 4, 6 }, { 0, 7, 8, 9 } };
+        int[] expected = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        int[] actual = main.mergeKSortedArrays(arrays);
+        assertArrayEquals(expected, actual);
     }
 }
