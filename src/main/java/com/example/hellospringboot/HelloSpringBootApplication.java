3a9c49ceef723d6ac57ea3d5eadb229641fb2f05
peanut996 <peanut996@icloud.com>
2025-09-08 15:57:33 +0800

    configure Eureka server and update application properties

--- src/main/java/com/example/hellospringboot/HelloSpringBootApplication.java
+++ src/main/java/com/example/hellospringboot/HelloSpringBootApplication.java
@@ -2,8 +2,10 @@ package com.example.hellospringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.ComponentScan;
 
+@EnableEurekaServer
 @ComponentScan("com.example.hellospringboot.*")
 @SpringBootApplication
 public class HelloSpringBootApplication {
